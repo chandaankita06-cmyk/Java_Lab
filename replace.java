f9ce3fdb6da681a3190eda39cf88c1eea9135870
chandaankita06-cmyk <chandaankita06@gmail.com>
2026-04-17 16:19:04 +0530

    Create replace.java

---
+++ replace.java
@@ -0,0 +1,11 @@
+public class ReplaceCharacters {
+    public static void main(String[] args) {
+        String original = "Today is a good day";
+        
+        // Replace all 'd' with 'f'
+        String modified = original.replace('d', 'f');
+        
+        System.out.println("Original String: " + original);
+        System.out.println("Modified String: " + modified);
+    }
+}
